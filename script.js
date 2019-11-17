09132b458dc1241446a5e7978c7a48ad2200fbb0
elenaserova <elein.serova@gmail.com>
2019-11-16 19:57:26 -0500

    js scroing fixed

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ quizApp.questions = [
             d: "Answer4"
 
         },
-        correctAnswer: "b"
+        correctAnswer: "a"
     },
     {
         question: "Question2?",
@@ -21,7 +21,7 @@ quizApp.questions = [
             d: "Answer4"
 
         },
-        correctAnswer: "d"
+        correctAnswer: "a"
     },
     {
         question: "Question3?",
@@ -58,8 +58,6 @@ quizApp.questions = [
     }
 ];
 
-quizApp.currentSlide = 0;
-quizApp.display = []
 
 //Selectors
 quizApp.quizContainer = document.querySelector(".quizContainer");
@@ -69,128 +67,131 @@ quizApp.startButton = document.getElementById('start');
 quizApp.submitButton = document.getElementById('submit');
 quizApp.againButton = document.getElementById('again');
 
-quizApp.displaySlide = function(slides, currentSlide) {
-    quizContainer.innerHTML = slides[currentSlide];
-} 
 
-quizApp.generateQuiz = function() {
+quizApp.correctAnswers = 0;
+
+quizApp.generateQuiz = function () {
+    
+    quizApp.currentQuestion = 0;
+    quizApp.display = [];
     quizApp.questions.forEach((currentQuestion, questionNumber) => {
         const answers = [];
         for (variant in currentQuestion.answers) {
-            answers.push(
+        answers.push(
                 `<label>
-                    <input type="radio" name="question${questionNumber}" value="${variant}">
+                    <input type="radio" name="question${questionNumber}" value="${variant}" class="radio">
                     ${variant} :
                     ${currentQuestion.answers[variant]}
                 </label>`
-            );
+        );
         }
         quizApp.display.push(
-            `<div class="slide"><div class="question"> ${currentQuestion.question} 
+        `<div class="slide"><div class="question"> ${currentQuestion.question} 
             </div>
                     <div class="answers"> ${answers.join("")} </div>
                     </div>`
         );
-    })
+    });
+
+    quizApp.quizContainer.innerHTML = quizApp.display[quizApp.currentQuestion];
+    quizApp.nextButton.style.display = "inline-block";
+    quizApp.startButton.style.display = "none";
+
     
-    quizApp.quizContainer = document.querySelector(".quizContainer");
-    quizApp.quizContainer.innerHTML = quizApp.display[quizApp.currentSlide];
 
-    quizApp.nextButtons = function () {
-        quizApp.nextButton.style.display = 'inline-block';
-        quizApp.startButton.style.display = 'none';
-
-
-    }
-    quizApp.nextButtons();
-    
 }
 
+quizApp.checkAnswer = function() {
+  const radios = Array.from(document.querySelectorAll(".radio"));
+  console.log(radios);
+  let radioValue;
+  for (let i = 0; i < radios.length; i++) {
+    if (radios[i].checked) {
+      radioValue = radios[i].value;
+      console.log(radios[i].value);
+    }
+  }
+  if (radioValue == null) {
+    alert("Please select an answer!");
+    quizApp.currentQuestion = quizApp.currentQuestion - 1;
+
+    
+  }
+
+  if (radioValue === quizApp.questions[quizApp.currentQuestion].correctAnswer) {
+    quizApp.correctAnswers++;
+  }
+  console.log(quizApp.questions[quizApp.currentQuestion].correctAnswer);
+};
 
 
-quizApp.finalScore = function() {
-    
-    const answerContainers = quizApp.quizContainer.querySelectorAll(".answers");
-    
-    
-    let correctAnswers = 0;
-    const testList = quizApp.questions;
-    testList.forEach(function(currentQuestion, questionNumber) {
-        
-        let answerContainer = answerContainers[questionNumber];
-        let selected = `input[name=question${questionNumber}]:checked`;
-        
-        let userAnswer = (answerContainer.querySelector(selected)).value;
-        if (userAnswer === currentQuestion.correctAnswer) {
-            correctAnswers++;
 
-        }
-        let totalScore = (correctAnswers / quizApp.questions.length) * 100;
-        console.log (totalScore);
+quizApp.showNextQuestion = function() {
+    
+
+    quizApp.checkAnswer();
+    
+  
+
+  quizApp.quizContainer.innerHTML = "";
+  quizApp.currentQuestion = quizApp.currentQuestion + 1;
+  $(quizApp.quizContainer).append(quizApp.display[quizApp.currentQuestion]);
+
+  if (quizApp.currentQuestion === quizApp.display.length - 1) {
+    quizApp.nextButton.style.display = "none";
+    quizApp.submitButton.style.display = "inline-block";
+    
+  } else {
+    quizApp.nextButton.style.display = "inline-block";
+    quizApp.submitButton.style.display = "none";
+  }
+};
+
+
+
+quizApp.finalScore = function () {
+
+        let totalScore = (quizApp.correctAnswers / quizApp.questions.length) * 100;
+        console.log(totalScore);
         $('.scoreContainer').text(`Your score is ${totalScore}%`);
         $('.scoreContainer').append('<p><button class="again" id="again">Try again</button></p>');
+        
         $('.again').on("click", function () {
             $('input').empty();
             $('.scoreContainer').empty();
+            quizApp.correctAnswers = 0;
             quizApp.quizContainer.style.display = 'inline-block';
+            quizApp.questions.sort(() => Math.random() - 0.5);
             quizApp.generateQuiz();
         });
 
-        
+  
+    }
 
 
-    });
-}
+    quizApp.submit = function () {
 
-
-quizApp.showNextSlide = function(){
-    quizApp.currentSlide = quizApp.currentSlide +1;
-    $(quizApp.quizContainer).append(quizApp.display[quizApp.currentSlide]);
+        quizApp.checkAnswer();
     
-    // i need to hide each previous Q
-    // $(quizApp.quizContainer).hide(quizApp.display[quizApp.previousSlide]);
+        quizApp.quizContainer.style.display = "none";
+        quizApp.scoreContainer.style.display = "inline-block";
+        quizApp.submitButton.style.display = "none";
 
-    if (quizApp.currentSlide === quizApp.display.length - 1) {
-        quizApp.nextButton.style.display = 'none';
-        quizApp.submitButton.style.display = 'inline-block';
-    }
-    
-    else {
-        quizApp.nextButton.style.display = 'inline-block';
-        quizApp.submitButton.style.display = 'none';
-        
-    }
+        quizApp.finalScore();
 
     }
+
+
 
 
 init = () => {
+    
     $('.start').on("click", quizApp.generateQuiz);
-    $('.submit').on("click", function(){
-        quizApp.quizContainer.style.display = 'none';
-        quizApp.scoreContainer.style.display = 'inline-block';
-        quizApp.finalScore();
-    });
-    $('.next').on("click", quizApp.showNextSlide);
-    // $('.again').on("click", quizApp.generateQuiz);
+    $('.next').on("click", quizApp.showNextQuestion);
+    $(".submit").on("click", quizApp.submit);
+
 }
 
 $(document).ready(function () {
     init();
-})
-
-
-
-
-
-
-
-
-        //     1.	User clicks “Start the quiz” button, then Q1 appears
-        //     2.	display question 1 and the set of choices to that question
-        //     3.	user should make a choice(click the button)
-        //     4.	if the choice is correct( === correct answer), then add  1 point to the finalScore, else add 0 to the finalPoint
-        //     5.	once the choice is made(button clicked), current Q disappears(hide method)
-        //     6.	if current Question < questionArray.length then move to the next question and set of choices, else display section with total score in %
-        //         7.	offer to start again(button)
-
+});
