0cb5bf2250719e1d5b9293682fc9b3f68e2de775
elenaserova <elein.serova@gmail.com>
2019-11-16 23:29:20 -0500

    styles updated

--- script.js
+++ script.js
@@ -1,137 +1,118 @@
 const quizApp = {};
 
 quizApp.questions = [
-    {
-        question: "Question1?",
-        answers: {
-            a: "Answer1",
-            b: "Answer2",
-            c: "Answer3",
-            d: "Answer4"
-
-        },
-        correctAnswer: "a"
+  {
+    question: "Which is the correct CSS syntax?",
+    answers: {
+      a: "{h1:color=yellow(p)}",
+      b: "h1 {color:yellow};",
+      c: "{h1;color:yellow}",
+      d: "h1:color=yellow"
     },
-    {
-        question: "Question2?",
-        answers: {
-            a: "Answer1",
-            b: "Answer2",
-            c: "Answer3",
-            d: "Answer4"
-
-        },
-        correctAnswer: "a"
+    correctAnswer: "b"
+  },
+  {
+    question: "Which snippet of CSS do you use to center website horizontally?",
+    answers: {
+      a: "margin: 0 auto;",
+      b: "padding: 0 auto;",
+      c: "margin: auto 0;",
+      d: "padding: auto 0;"
     },
-    {
-        question: "Question3?",
-        answers: {
-            a: "Answer1",
-            b: "Answer2",
-            c: "Answer3",
-            d: "Answer4"
-
-        },
-        correctAnswer: "a"
+    correctAnswer: "a"
+  },
+  {
+    question:
+      "Which HTML attribute specifies an alternate text for an image, if the image cannot be displayed?",
+    answers: {
+      a: "src",
+      b: "alt",
+      c: "title",
+      d: "attr"
     },
-    {
-        question: "Question4?",
-        answers: {
-            a: "Answer1",
-            b: "Answer2",
-            c: "Answer3",
-            d: "Answer4"
-
-        },
-        correctAnswer: "c"
+    correctAnswer: "b"
+  },
+  {
+    question:
+      "Which of the following would be used to create an ID called mainContainer which has a width of 600px, a height of 100px and the color of the text is red?",
+    answers: {
+      a: "#mainContainer {height:100px; width: 600px; text:black;}",
+      b: ".mainContainer {height:100px; width: 600px; colour:black;}",
+      c: "#mainContainer {height:100px; width: 600px; color:black;}",
+      d: ".mainContainer {height:100px; width: 600px; color:black;}"
     },
-    {
-        question: "Question5?",
-        answers: {
-            a: "Answer1",
-            b: "Answer2",
-            c: "Answer3",
-            d: "Answer4"
-
-        },
-        correctAnswer: "b"
-    }
+    correctAnswer: "c"
+  },
+  {
+    question: "Which tag is used to let control over the viewport?",
+    answers: {
+      a: "address",
+      b: "meta",
+      c: "link",
+      d: "acronym"
+    },
+    correctAnswer: "b"
+  }
 ];
-
 
 //Selectors
 quizApp.quizContainer = document.querySelector(".quizContainer");
 quizApp.scoreContainer = document.querySelector(".scoreContainer");
-quizApp.nextButton = document.getElementById('next');
-quizApp.startButton = document.getElementById('start');
-quizApp.submitButton = document.getElementById('submit');
-quizApp.againButton = document.getElementById('again');
-
+quizApp.nextButton = document.getElementById("next");
+quizApp.startButton = document.getElementById("start");
+quizApp.submitButton = document.getElementById("submit");
+quizApp.againButton = document.getElementById("again");
 
 quizApp.correctAnswers = 0;
 
-quizApp.generateQuiz = function () {
-    
-    quizApp.currentQuestion = 0;
-    quizApp.display = [];
-    quizApp.questions.forEach((currentQuestion, questionNumber) => {
-        const answers = [];
-        for (variant in currentQuestion.answers) {
-        answers.push(
-                `<label>
+quizApp.generateQuiz = function() {
+  $(".rules").hide();
+  quizApp.currentQuestion = 0;
+  quizApp.display = [];
+  quizApp.questions.forEach((currentQuestion, questionNumber) => {
+    const answers = [];
+    for (variant in currentQuestion.answers) {
+      answers.push(
+        `<label>
                     <input type="radio" name="question${questionNumber}" value="${variant}" class="radio">
-                    ${variant} :
+      
                     ${currentQuestion.answers[variant]}
                 </label>`
-        );
-        }
-        quizApp.display.push(
-        `<div class="slide"><div class="question"> ${currentQuestion.question} 
+      );
+    }
+    quizApp.display.push(
+      `<div class="slide"><div class="question"> ${currentQuestion.question} 
             </div>
                     <div class="answers"> ${answers.join("")} </div>
                     </div>`
-        );
-    });
+    );
+  });
 
-    quizApp.quizContainer.innerHTML = quizApp.display[quizApp.currentQuestion];
-    quizApp.nextButton.style.display = "inline-block";
-    quizApp.startButton.style.display = "none";
-
-    
-
-}
+  quizApp.quizContainer.innerHTML = quizApp.display[quizApp.currentQuestion];
+  quizApp.nextButton.style.display = "inline-block";
+  quizApp.startButton.style.display = "none";
+};
 
 quizApp.checkAnswer = function() {
   const radios = Array.from(document.querySelectorAll(".radio"));
-  console.log(radios);
   let radioValue;
   for (let i = 0; i < radios.length; i++) {
     if (radios[i].checked) {
       radioValue = radios[i].value;
-      console.log(radios[i].value);
     }
   }
   if (radioValue == null) {
     alert("Please select an answer!");
     quizApp.currentQuestion = quizApp.currentQuestion - 1;
-
-    
   }
 
   if (radioValue === quizApp.questions[quizApp.currentQuestion].correctAnswer) {
     quizApp.correctAnswers++;
   }
-  console.log(quizApp.questions[quizApp.currentQuestion].correctAnswer);
 };
 
-
-
 quizApp.showNextQuestion = function() {
-    
-
-    quizApp.checkAnswer();
-    
-  
+  quizApp.checkAnswer();
 
   quizApp.quizContainer.innerHTML = "";
   quizApp.currentQuestion = quizApp.currentQuestion + 1;
@@ -140,58 +121,45 @@ quizApp.showNextQuestion = function() {
   if (quizApp.currentQuestion === quizApp.display.length - 1) {
     quizApp.nextButton.style.display = "none";
     quizApp.submitButton.style.display = "inline-block";
-    
   } else {
     quizApp.nextButton.style.display = "inline-block";
     quizApp.submitButton.style.display = "none";
   }
 };
 
+quizApp.finalScore = function() {
+  let totalScore = (quizApp.correctAnswers / quizApp.questions.length) * 100;
+  $(".scoreContainer").text(`Your score is ${totalScore}%`);
+  $(".scoreContainer").append(
+    '<p><button class="again" id="again">Try again</button></p>'
+  );
 
+  $(".again").on("click", function() {
+    $("input").empty();
+    $(".scoreContainer").empty();
+    quizApp.correctAnswers = 0;
+    quizApp.quizContainer.style.display = "inline-block";
+    quizApp.questions.sort(() => Math.random() - 0.5);
+    quizApp.generateQuiz();
+  });
+};
 
-quizApp.finalScore = function () {
+quizApp.submit = function() {
+  quizApp.checkAnswer();
 
-        let totalScore = (quizApp.correctAnswers / quizApp.questions.length) * 100;
-        console.log(totalScore);
-        $('.scoreContainer').text(`Your score is ${totalScore}%`);
-        $('.scoreContainer').append('<p><button class="again" id="again">Try again</button></p>');
-        
-        $('.again').on("click", function () {
-            $('input').empty();
-            $('.scoreContainer').empty();
-            quizApp.correctAnswers = 0;
-            quizApp.quizContainer.style.display = 'inline-block';
-            quizApp.questions.sort(() => Math.random() - 0.5);
-            quizApp.generateQuiz();
-        });
+  quizApp.quizContainer.style.display = "none";
+  quizApp.scoreContainer.style.display = "inline-block";
+  quizApp.submitButton.style.display = "none";
 
-  
-    }
-
-
-    quizApp.submit = function () {
-
-        quizApp.checkAnswer();
-    
-        quizApp.quizContainer.style.display = "none";
-        quizApp.scoreContainer.style.display = "inline-block";
-        quizApp.submitButton.style.display = "none";
-
-        quizApp.finalScore();
-
-    }
-
-
-
+  quizApp.finalScore();
+};
 
 init = () => {
-    
-    $('.start').on("click", quizApp.generateQuiz);
-    $('.next').on("click", quizApp.showNextQuestion);
-    $(".submit").on("click", quizApp.submit);
+  $(".start").on("click", quizApp.generateQuiz);
+  $(".next").on("click", quizApp.showNextQuestion);
+  $(".submit").on("click", quizApp.submit);
+};
 
-}
-
-$(document).ready(function () {
-    init();
+$(document).ready(function() {
+  init();
 });
